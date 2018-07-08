654ee0fbfb088a9894166fdc818fbed2f67c8129
pelev1n <pelevin9911@gmail.com>
2018-07-08 16:09:35 +0300

    Removed void main

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,6 +1,3 @@
 public class Program {
-    public static void main(String[] args) {
-        Triangle tr6 = new Triangle(new Point(1,1),new Point(-2,-5),new Point(3,0));
-        System.out.println(tr6.isRightTriangle());
-    }
+
 }
